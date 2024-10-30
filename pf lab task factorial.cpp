9212f22a95997d549aecef2ef4871bd006283379
rameisabaqir <MeesaBaqir369@gmail.com>
2024-10-29 21:55:18 -0700

    Add files via upload

---
+++ pf lab task factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//5! 5*4*3*2*1=120
+	int n;
+	long factorial = 1.0;
+	cout<<"Enter factorial";
+	cin>>n;
+	if(n<0)
+	cout<<"Error! Factorial of negative number doesn't exist";
+	else{
+		int i=1;
+		do{
+			cout<<i<<endl;
+			i++;
+		}
+		while(i<=n);
+		cout<<"factorial of"<<n<<"="<<factorial;
+	}
+	return 0;
+}
